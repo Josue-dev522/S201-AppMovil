81c22e0ae36334d1b5bb850ac4a07f8222c901b0
Josue-dev522 <122043884@upq.edu.mx>
2026-06-19 22:47:31 -0600

    Practica9

--- App.js
+++ App.js
@@ -1,33 +1,23 @@
-//Zona 1: Importaciones de archivos y Componentes
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { Perfil } from './components/Perfil';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import MenuScreen from './Screens/MenuScreen';
 
-//Zona 2: Main - componentes 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <Perfil style={styles.tarjetaVerde} nombre="Hernandez Cruz Hector Josue " carrera="Ingeniería en Sistemas Computacionales" materia="Programación Móvil" cuatrimestre="9" />
-      <Perfil style={styles.tarjetaRoja} nombre="Jonathan Carbajal Reyes" carrera="Ingeniería en Sistemas Computacionales" materia="Programación Movil" cuatrimestre="9" />
-      <Perfil style={styles.tarjetaRoja} nombre="Jonathan Carbajal Reyes" carrera="Ingeniería en Sistemas Computacionales" materia="Programación Movil" cuatrimestre="9" />
-
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <MenuScreen></MenuScreen>
+    </SafeAreaProvider>
   );
 }
 
-//Zona 3: Estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    backgroundColor: '#f35858',
+
     flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
   },
-
-  tarjetaVerde:{backgroundColor: 'green',},
-  tarjetaRoja:{backgroundColor: 'red',},
-
 });
---
+++ components/menuScreen.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { Button, StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Componente1 from './Componente1';
+import TarjetasScreen from './TarjetasScreen';
+
+
+
+export default function App() {
+    const[screen, setScreen]= useState('menu');
+    
+    switch(screen){
+        case 'tarjetas':
+            return <TarjetasScreen/>
+        case 'componente1':
+            return <Componente1/>
+        case 'menu':
+            default:
+            return (
+                <SafeAreaView style={styles.container}>
+                    <Text style={styles.titulo}>Menú Prácticas</Text>
+
+                    <Button title='Practica Tarjetas' onPress={()=>setScreen('tarjetas')}></Button>
+                    <Button title='Practica Componente1' onPress={()=>setScreen('componente1')}></Button>
+                </SafeAreaView>
+            ); 
+    } 
+} 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff', 
+    flexDirection: 'column', 
+    justifyContent: 'flex-start', 
+    alignItems: 'center', 
+    gap: 20, 
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  }
+});
